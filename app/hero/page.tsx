12cd5aa0d65e4228a7eb7a8e429b5648168725f5
klawcodes <codenamezeskil@gmail.com>
2024-02-11 15:03:07 +0700

    animations & breakpoints 640px

--- app/hero/page.tsx
+++ app/hero/page.tsx
@@ -2,17 +2,29 @@
 
 import { ChangeEvent, useEffect, useState } from "react";
 import Select from "react-select";
-import Textra from 'react-textra'
-import Image from 'next/image'
+import Textra from "react-textra";
+import Image from "next/image";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const Hero = () => {
-  const [locations, setLocations] = useState<{ value: string; label: string }[]>([]);
-  const [selectedLocation, setSelectedLocation] = useState<{ value: string; label: string } | null>({
+  useEffect(() => {
+    AOS.init();
+    AOS.refresh();
+  }, []);
+
+  const [locations, setLocations] = useState<
+    { value: string; label: string }[]
+  >([]);
+  const [selectedLocation, setSelectedLocation] = useState<{
+    value: string;
+    label: string;
+  } | null>({
     value: "1301", // ID Kota Jakarta
     label: "Kota Jakarta",
   });
   const [prayerSchedule, setPrayerSchedule] = useState<any>(null);
-  
+
   useEffect(() => {
     // Fetch data from API
     const fetchData = async () => {
@@ -32,23 +44,23 @@ const Hero = () => {
         console.error("Error fetching data:", error);
       }
     };
-  
+
     fetchData();
   }, []);
-  
+
   useEffect(() => {
     // Fetch prayer schedule for Jakarta after component mounts
     if (selectedLocation) {
       fetchPrayerSchedule(selectedLocation.value);
     }
   }, [selectedLocation]);
-  
+
   const fetchPrayerSchedule = async (idKota: string) => {
     const currentDate = new Date();
     const year = currentDate.getFullYear();
     const month = currentDate.getMonth() + 1; // Bulan dimulai dari 0 (Januari)
     const day = currentDate.getDate();
-  
+
     try {
       const url = `https://api.myquran.com/v2/sholat/jadwal/${idKota}/${year}/${month}/${day}`;
       const response = await fetch(url);
@@ -62,7 +74,7 @@ const Hero = () => {
       console.error("Error fetching prayer schedule:", error);
     }
   };
-  
+
   const handleLocationChange = (
     selectedOption: { value: string; label: string } | null
   ) => {
@@ -71,11 +83,9 @@ const Hero = () => {
       fetchPrayerSchedule(selectedOption.value); // Panggil fungsi fetchPrayerSchedule dengan idKota terpilih
     }
   };
-  
-
 
   const [currentTime, setCurrentTime] = useState(new Date());
-  const [localTimeZone, setLocalTimeZone] = useState('');
+  const [localTimeZone, setLocalTimeZone] = useState("");
 
   useEffect(() => {
     const timer = setInterval(() => {
@@ -83,12 +93,28 @@ const Hero = () => {
     }, 1000);
 
     // Mendapatkan offset dari UTC
-    const offset = currentTime.getTimezoneOffset() / 60;
+    const offsetMinutes = -currentTime.getTimezoneOffset();
+    const offsetHours = Math.floor(offsetMinutes / 60);
 
-    // Konversi ke format GMT
-    const gmtString = offset >= 0 ? `GMT+${offset}` : `GMT${offset}`;
+    // Set zona waktu lokal
+    let timeZone = "";
+    if (offsetHours === 7) {
+      timeZone = "WIB";
+    } else if (offsetHours === 8) {
+      timeZone = "WITA";
+    } else if (offsetHours === 9) {
+      timeZone = "WIT";
+    }
 
-    setLocalTimeZone(gmtString);
+    // Format waktu
+    const formattedTime = `${formatTime(currentTime.getHours())}:${formatTime(
+      currentTime.getMinutes()
+    )}:${formatTime(currentTime.getSeconds())}`;
+
+    // Set keterangan zona waktu lokal
+    const timeZoneInfo = `GMT+${offsetHours} | ${formattedTime} ${timeZone}`;
+
+    setLocalTimeZone(timeZoneInfo);
 
     return () => {
       clearInterval(timer);
@@ -97,20 +123,25 @@ const Hero = () => {
 
   const formatTime = (time: number) => {
     return time < 10 ? `0${time}` : time.toString();
-};
-
-  const [isLoading, setIsLoading] = useState(false);
-
+  };
 
   return (
-    <div className="px-[3.5rem] py-[1rem] flex justify-between items-center h-screen">
+    <div className="px-[3.5rem] py-[1rem] flex max-[640px]:flex-col max-[640px]:py-[2rem] max-[640px]:space-y-10 justify-between items-center h-screen">
       <div className="flex flex-col">
         <Image
           src="/img/bunderan.png"
-          width={200}
-          height={200}
+          sizes="100vw"
+          style={{
+            width: "30%",
+            height: "auto",
+          }}
+          width={250}
+          height={250}
           alt="lantern"
-          className="rounded-3xl"
+          className="rounded-3xl max-[640px]:mb-5"
+          data-aos="fade-up"
+          data-aos-delay="800"
+          data-aos-duration="1000"
         />
 
         <p
@@ -120,89 +151,180 @@ const Hero = () => {
           to-[#aad576] 
           inline-block 
           monas
-          text-[8rem] 
+          text-[8rem]
+          max-[640px]:text-[5.5rem]
           text-transparent 
           bg-clip-text
           drop-shadow-xl
-          leading-[7rem]"
+          leading-[7rem]
+          max-[640px]:leading-[4.5rem]"
+          data-aos="fade-up"
+          data-aos-delay="1000"
+          data-aos-duration="1000"
         >
           Awas <br /> Imsak!
         </p>
-        <Textra
-          effect="downTop"
-          duration={500}
-          data={[
-            "Awas Imsak! hadir untuk teman-teman yang suka sahur jam 12 siang",
-            "Masa puasa 30 hari doang ga bisa sih cuy?",
-            "Awas Imsak! Puasa Tenang, Hati Gembirang.",
-          ]}
-          className="opensans text-[15px]"
-        />
-        <p className="poppins-medium mt-8">Dari Abu Hurairah RA berkata, Rasulullah SAW bersabda: <br /> &quot;Siapa berpuasa di bulan Ramadan dengan dilandasi iman dan <br /> ikhlas mengharap ridha Allah, maka diampuni dosanya yang lalu,&quot; (HR Al-Bukhari)</p>
+        <div data-aos="fade-up" data-aos-delay="1200" data-aos-duration="1000">
+          <Textra
+            effect="downTop"
+            duration={500}
+            data={[
+              "Awas Imsak! hadir untuk teman-teman yang suka sahur jam 12 siang",
+              "Masa puasa 30 hari doang ga bisa sih cuy?",
+              "Awas Imsak! Puasa Tenang, Hati Gembirang.",
+            ]}
+            className="opensans text-[15px]"
+          />
+        </div>
+        <p
+          className="poppins-medium 
+        mt-8 max-[640px]:text-[13px]"
+          data-aos="fade-up"
+          data-aos-delay="1400"
+          data-aos-duration="1000"
+        >
+          Dari Abu Hurairah RA berkata, Rasulullah SAW bersabda: <br />{" "}
+          &quot;Siapa berpuasa di bulan Ramadan dengan dilandasi iman dan <br />{" "}
+          ikhlas mengharap ridha Allah, maka diampuni dosanya yang lalu,&quot;
+          (HR Al-Bukhari)
+        </p>
       </div>
 
-      <div className="flex flex-col w-[40rem]">
-        <div className="flex space-x-1 text-3xl mb-5 border border-[#0d2818] p-4 rounded-full text-center justify-center items-center bg-[#0d1811]">
-          <p className="opensans">{localTimeZone.replace("GMT-", "GMT +")}</p>
-          <p className="opensans">
-            {" "}
-            | {formatTime(currentTime.getHours())}:
-            {formatTime(currentTime.getMinutes())}:
-            {formatTime(currentTime.getSeconds())}
-          </p>
+      <div className="flex flex-col w-[40rem] max-[640px]:w-[20rem]">
+        <div
+          className="
+        flex 
+        space-x-1 
+        text-3xl
+        max-[640px]:text-lg
+        mb-5 
+        border 
+        border-[#0d2818] 
+        p-4 
+        rounded-full 
+        text-center justify-center items-center bg-[#0d1811]"
+          data-aos="fade-up"
+          data-aos-delay="1000"
+          data-aos-duration="1000"
+        >
+          <p className="opensans">{localTimeZone}</p>
         </div>
-        <h1 className="text-2xl poppins-extrabold mb-4">Pilih Wilayah:</h1>
-        <Select
-          options={locations}
-          value={selectedLocation}
-          onChange={handleLocationChange}
-          placeholder="Cari lokasi..."
-          isClearable
-          className="poppins-regular mb-4 hover:cursor-text text-black"
-        />
+        <h1
+          className="text-2xl poppins-extrabold mb-4 max-[640px]:text-lg"
+          data-aos="fade-up"
+          data-aos-delay="1100"
+          data-aos-duration="1000"
+        >
+          Pilih Wilayah:
+        </h1>
+        <div
+          data-aos="fade-up"
+          data-aos-delay="1200"
+          data-aos-duration="1000"
+          className="z-[999]"
+        >
+          <Select
+            options={locations}
+            value={selectedLocation}
+            onChange={handleLocationChange}
+            placeholder="Cari lokasi..."
+            isClearable
+            className="poppins-regular mb-4 hover:cursor-text text-black"
+          />
+        </div>
         {prayerSchedule && (
           <div className="poppins-regular text-[20px]">
             <div className="flex justify-between">
-              <div className="flex flex-col mb-5">
+              <div
+                className="flex flex-col mb-5 max-[640px]:text-lg"
+                data-aos="fade-up"
+                data-aos-delay="1300"
+                data-aos-duration="1000"
+              >
                 <span className="font-semibold">Wilayah:</span>
                 <span>{selectedLocation && selectedLocation.label}</span>
               </div>
-              <div className="flex flex-col mb-5">
+              <div
+                className="flex flex-col mb-5 max-[640px]:text-lg"
+                data-aos="fade-up"
+                data-aos-delay="1400"
+                data-aos-duration="1000"
+              >
                 <span className="font-semibold">Hari / Tanggal:</span>
                 <span>{prayerSchedule.tanggal}</span>
               </div>
             </div>
-            <div className="grid grid-cols-4 gap-4">
-              <div className="flex flex-col border border-[#0d2818] p-4 rounded-md active-box active-shadow">
+            <div className="grid grid-cols-4 gap-4 max-[640px]:grid-cols-2">
+              <div
+                className="flex flex-col border border-[#0d2818] p-4 rounded-md active-box active-shadow max-[640px]:text-lg"
+                data-aos="fade-up"
+                data-aos-delay="1400"
+                data-aos-duration="1000"
+              >
                 <span className="font-semibold">Imsak:</span>
                 <span>{prayerSchedule.imsak}</span>
               </div>
 
-              <div className="flex flex-col border border-[#0d2818] p-4 rounded-md active-shadow">
+              <div
+                className="flex flex-col border border-[#0d2818] p-4 rounded-md active-shadow max-[640px]:text-lg"
+                data-aos="fade-up"
+                data-aos-delay="1500"
+                data-aos-duration="1000"
+              >
                 <span className="font-semibold">Subuh:</span>
                 <span>{prayerSchedule.subuh}</span>
               </div>
-              <div className="flex flex-col border border-[#0d2818] p-4 rounded-md active-shadow">
+              <div
+                className="flex flex-col border border-[#0d2818] p-4 rounded-md active-shadow max-[640px]:text-lg"
+                data-aos="fade-up"
+                data-aos-delay="1600"
+                data-aos-duration="1000"
+              >
                 <span className="font-semibold">Terbit:</span>
                 <span>{prayerSchedule.terbit}</span>
               </div>
-              <div className="flex flex-col border border-[#0d2818] p-4 rounded-md active-shadow">
+              <div
+                className="flex flex-col border border-[#0d2818] p-4 rounded-md active-shadow max-[640px]:text-lg"
+                data-aos="fade-up"
+                data-aos-delay="1700"
+                data-aos-duration="1000"
+              >
                 <span className="font-semibold">Dhuha:</span>
                 <span>{prayerSchedule.dhuha}</span>
               </div>
-              <div className="flex flex-col border border-[#0d2818] p-4 rounded-md active-shadow">
+              <div
+                className="flex flex-col border border-[#0d2818] p-4 rounded-md active-shadow max-[640px]:text-lg"
+                data-aos="fade-up"
+                data-aos-delay="1800"
+                data-aos-duration="1000"
+              >
                 <span className="font-semibold">Dzuhur:</span>
                 <span>{prayerSchedule.dzuhur}</span>
               </div>
-              <div className="flex flex-col border border-[#0d2818] p-4 rounded-md active-shadow">
+              <div
+                className="flex flex-col border border-[#0d2818] p-4 rounded-md active-shadow max-[640px]:text-lg"
+                data-aos="fade-up"
+                data-aos-delay="1900"
+                data-aos-duration="1000"
+              >
                 <span className="font-semibold">Ashar:</span>
                 <span>{prayerSchedule.ashar}</span>
               </div>
-              <div className="flex flex-col border border-[#0d2818] p-4 rounded-md active-box active-shadow">
+              <div
+                className="flex flex-col border border-[#0d2818] p-4 rounded-md active-box active-shadow max-[640px]:text-lg"
+                data-aos="fade-up"
+                data-aos-delay="2000"
+                data-aos-duration="1000"
+              >
                 <span className="font-semibold">Maghrib:</span>
                 <span>{prayerSchedule.maghrib}</span>
               </div>
-              <div className="flex flex-col border border-[#0d2818] p-4 rounded-md active-shadow">
+              <div
+                className="flex flex-col border border-[#0d2818] p-4 rounded-md active-shadow max-[640px]:text-lg"
+                data-aos="fade-up"
+                data-aos-delay="2100"
+                data-aos-duration="1000"
+              >
                 <span className="font-semibold">Isya:</span>
                 <span>{prayerSchedule.isya}</span>
               </div>
